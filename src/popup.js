06eb4194e228809c7f76bc2b4e073e85def9aa2a
shobhit singh <shobhitsingh29@gmail.com>
2020-04-26 01:53:03 +0530

    chneg

--- src/popup.js
+++ src/popup.js
@@ -10,16 +10,8 @@ document.addEventListener(
         var noderes = document.getElementById("result");
         var textContent = node.value;
         console.oldLog = console.log;
-
-        console.log = function(value) {
-          console.oldLog(value);
-          return value;
-        };
         const output = eval(textContent);
         noderes.value = output;
-        chrome.tabs.getSelected(null, function(tab) {
-          alert(output);
-        });
       },
       false
     );
